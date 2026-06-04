e8e1e2047db2d43cf45a0242a31e3f25f52b6447
artem <zashl03@yandex.ru>
2026-06-05 02:51:56 +0300

    Added category tests

--- tests/ProToolRent.Domain.Tests/Entities/CategoryTests.cs
+++ tests/ProToolRent.Domain.Tests/Entities/CategoryTests.cs
@@ -1,6 +1,37 @@
-﻿namespace ProToolRent.Domain.tests;
+﻿using ProToolRent.Domain.Entities;
+
+namespace ProToolRent.Domain.tests;
 
 public class CategoryTests
 {
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Constructor_WhenNameIsInvalid_ThrowsException(string? name)
+    {
+        var ex = Assert.Throws<ArgumentException>(() => new Category(name!));
 
+        Assert.Equal("name", ex.ParamName);
+    }
+
+    [Fact]
+    public void Constructor_WhenValidName_CreatesRootCategory()
+    {
+        var category = new Category("Tools");
+
+        Assert.Equal("Tools", category.Name);
+        Assert.Null(category.ParentId);
+        Assert.Null(category.Parent);
+    }
+
+    [Fact]
+    public void Constructor_WithParent_CreatesChildCategory()
+    {
+        var parent = new Category("Tools");
+        var child = new Category("Hand tools", parent.Id, parent);
+
+        Assert.Equal(parent.Id, child.ParentId);
+        Assert.Equal(parent, child.Parent);
+    }
 }
