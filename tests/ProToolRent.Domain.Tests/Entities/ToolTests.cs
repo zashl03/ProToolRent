7df78c5fdbaf202ec7c31325f4e78c1c41ecdbc8
artem <zashl03@yandex.ru>
2026-06-06 00:45:47 +0300

    Added tool tests

---
+++ tests/ProToolRent.Domain.Tests/Entities/ToolTests.cs
@@ -0,0 +1,51 @@
+﻿using ProToolRent.Domain.Entities;
+using ProToolRent.Domain.ValueObjects;
+
+namespace ProToolRent.Domain.tests;
+
+public class ToolTests
+{
+    [Fact]
+    public void Constructor_WhenPriceIsInvalid_ThrowsException()
+    {
+        var specification = new Specification("Brand", "Name", 100);
+        var quantity = new Quantity(10);
+
+        var ex = Assert.Throws<ArgumentException>(() => new Tool(
+            specification,
+            quantity,
+            "description",
+            -5,
+            Guid.NewGuid(),
+            Guid.NewGuid()
+            ));
+        
+        Assert.Equal("price", ex.ParamName);
+    }
+
+    [Fact]
+    public void Constructor_WhenDataIsValid_CreatesToolObject()
+    {
+        var specification = new Specification("Brand", "Name", 100);
+        var quantity = new Quantity(10);
+        var categoryId = Guid.NewGuid();
+        var userId = Guid.NewGuid();
+
+
+        var tool = new Tool(
+            specification,
+            quantity,
+            "description",
+            100,
+            categoryId,
+            userId
+            );
+        
+        Assert.Equal("description", tool.Description);
+        Assert.Equal(100, tool.Price);
+        Assert.Equal(categoryId, tool.CategoryId);
+        Assert.Equal(userId, tool.UserId);
+        Assert.Equal(specification, tool.Specification);
+        Assert.Equal(quantity, tool.Quantity);
+    }
+}
